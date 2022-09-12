29511567f40efedbdb90982265ab6e24663961e6
Benjie-Sena-Azumah <Azumahsena1@gmail.com>
2022-09-12 16:48:31 -0700

    puts commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/** use the put function to print
+ * donot use the printf function
+ *
+ */
+int main(void)
+{       
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
+
+
