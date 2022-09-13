fcc8c811e274170fb72ad082766eab7d1edab668
Benjie-Sena-Azumah <Azumahsena1@gmail.com>
2022-09-12 18:36:12 -0700

    quote commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+* entry point of the code
+* description - we use everything
+
+*/
+int main(void)
+{
+	Char error[] = {“and" and that piece of art is useful""
+		" - Dora korpa, 2015-10-15\n"};
+	write(2, error, 59);
+	return (1);
+}
